91f937be18b7c4d2015480f4d2def6d98d6099d6
Lucas Canuto <lucas.canuto@sptech.school>
2024-06-03 10:57:07 -0300

    Limpando arquivos desnecessarios e toques finais no projeto

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -11,9 +11,6 @@ var app = express();
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
 var publicacaoRouter = require("./src/routes/publicacao")
 var pesquisaRouter = require("./src/routes/pesquisa")
 
@@ -26,9 +23,6 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
 app.use("/publicacao", publicacaoRouter);
 app.use("/pesquisa", pesquisaRouter);
 
